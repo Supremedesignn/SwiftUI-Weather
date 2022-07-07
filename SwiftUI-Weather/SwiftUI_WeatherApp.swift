af7db387dabc7e989c9f7c6cbc3832bb92164115
Nkosi Yafeu <95482668+Supremedesignn@users.noreply.github.com>
2022-07-06 20:58:15 -0400

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Nkosi Yafeu on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
